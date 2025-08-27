e43e37c262c53967d85afb6e46d605ddeab558b7
annap <ap.mesa@alumno.um.edu.ar>
2025-08-26 23:32:14 -0300

    feat : inicializar proyecto con estructura basica  "Agregué a el archivo mazmorras.py con el mapa y el movimiento básico"

---
+++ mazmorras.py
@@ -0,0 +1,37 @@
+def mostrar_mapa(mapa, jugador_pos):
+    """
+    Muestra el mapa en consola con el jugador en su posición actual.
+    @ representa al jugador
+    """
+    for i, fila in enumerate(mapa):
+        fila_str = ""
+        for j, casilla in enumerate(fila):
+            if (i, j) == jugador_pos:
+                fila_str += "@ "
+            else:
+                fila_str += casilla + " "
+        print(fila_str)
+    print()
+
+
+def mover_jugador(direccion, jugador_pos, mapa):
+    """
+    Mueve al jugador según la tecla ingresada:
+    w = arriba, s = abajo, a = izquierda, d = derecha
+    """
+    x, y = jugador_pos
+    filas = len(mapa)
+    columnas = len(mapa[0])
+
+    if direccion == "w" and x > 0:  # arriba
+        x -= 1
+    elif direccion == "s" and x < filas - 1:  # abajo
+        x += 1
+    elif direccion == "a" and y > 0:  # izquierda
+        y -= 1
+    elif direccion == "d" and y < columnas - 1:  # derecha
+        y += 1
+    else:
+        print("Movimiento inválido.")
+
+    return (x, y)
