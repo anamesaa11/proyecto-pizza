3a4c505286c048eace8807e477513193f3ece320
Mia <mivvma@gmail.com>
2025-09-03 12:55:07 -0300

    Creación Enemigos: atributos y funciones básicas.

---
+++ enemigos.py
@@ -0,0 +1,33 @@
+import random
+#from Personajes import Personaje
+
+
+class Enemigo:
+    def __init__(self, nombre, nivel, vida, ataque, defensa, velocidad, experiencia, comportamiento, habilidad):
+        self.nombre = nombre
+        self.nivel = nivel
+        self.vida = vida
+        self.ataque = ataque
+        self.defensa = defensa
+        self.velocidad = velocidad
+        self.experiencia = experiencia
+        self.comportamiento = comportamiento
+        self.habilidad = habilidad
+
+    def con_vida(self):
+        return self.vida > 0
+
+    def recibir_dano(self, cantidad):
+        dano_real = max(0, cantidad - self.defensa)
+        self.vida = dano_real
+        print(f'{self.nombre} recibió {dano_real} de daño. | Vida restante: {self.vida}')
+        if self.vida <= 0:
+            print(f'{self.nombre} ha sido eliminado.')
+
+    def atacar(self):
+        dano = random.randint(int(self.ataque*0.8), int(self.ataque*1.2))
+        print(f'{self.nombre} ataca a (Personaje) | Daño impactado: {dano}')
+#        Personaje.recibir_dano()
+
+    def use_habilidad(self):
+        pass
