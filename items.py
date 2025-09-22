6534f483b4d30f206b91dfbfb25ddc2f4396961a
annap <ap.mesa@alumno.um.edu.ar>
2025-09-22 11:53:24 -0300

    agregue sistema de items(clase items y lista basica)

---
+++ items.py
@@ -0,0 +1,30 @@
+class Item:
+    def __init__(self, nombre, tipo, efecto, valor):
+        self.nombre = nombre
+        self.tipo = tipo  # consumible o equipable
+        self.efecto = efecto
+        self.valor = valor
+
+    def usar(self, personaje):
+        if self.efecto == "vida":
+            personaje.curar(self.valor)
+        elif self.efecto == "fuerza":
+            personaje.fuerza += self.valor
+            print(f"{personaje.nombre} gana +{self.valor} fuerza.")
+        elif self.efecto == "defensa":
+            personaje.defensa += self.valor
+            print(f"{personaje.nombre} gana +{self.valor} defensa.")
+        elif self.efecto == "velocidad":
+            personaje.velocidad += self.valor
+            print(f"{personaje.nombre} gana +{self.valor} velocidad.")
+        else:
+            print("Este ítem no tiene efecto.")
+
+# Ítems concretos
+pocion_vida = Item("Poción de Vida", "consumible", "vida", 30)
+espada = Item("Espátula de Hierro", "equipable", "fuerza", 5)
+escudo = Item("Caja de Cartón", "equipable", "defensa", 3)
+botas = Item("Zapatillas de Reparto", "equipable", "velocidad", 2)
+elixir = Item("Elixir de Queso Fundido", "consumible", "vida", 50)
+
+lista_items = [pocion_vida, espada, escudo, botas, elixir]
